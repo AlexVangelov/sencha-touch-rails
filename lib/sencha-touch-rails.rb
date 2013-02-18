7bf792acc5cc2608cccaa72c5895b2ed4d17108a
Lucjan Suski <lucjansuski@gmail.com>
2013-02-18 14:18:15 +0100

    Fixed paths

--- lib/sencha-touch-rails.rb
+++ lib/sencha-touch-rails.rb
@@ -1,6 +1,9 @@
 require 'sencha-touch/rails'
-
-require 'sass_load_paths'
-
 require 'theme_images'
 
+module SenchaTouch
+  def load!
+    stylesheets = File.expand_path(File.join("..", 'vendor', 'assets', 'stylesheets'))
+    Sass.load_paths << stylesheets
+  end
+end
--- lib/sencha-touch/rails/version.rb
+++ lib/sencha-touch/rails/version.rb
@@ -1,6 +1,6 @@
 module SenchaTouch
   module Rails
-    VERSION = "1.1.1"
+    VERSION = "1.1.2"
     SENCHA_TOUCH_VERSION = "2.1.1"
   end
 end
