e734182cc723c77f2871d462933439f87ef88630
Lucjan Suski <lucjansuski@gmail.com>
2013-02-19 15:56:08 +0100

    Fixed error

--- lib/sencha-touch-rails.rb
+++ lib/sencha-touch-rails.rb
@@ -2,9 +2,10 @@ require 'sencha-touch/rails'
 require 'theme_images'
 
 module SenchaTouch
-  def load!
-    stylesheets = File.expand_path(File.join("..", 'vendor', 'assets', 'stylesheets'))
-    Sass.load_paths << stylesheets
+  def self.load!
+    path = File.expand_path File.dirname(__FILE__)
+    Sass.load_paths << File.expand_path(File.join(path, "..", 'vendor', 'assets', 'stylesheets'))
+    Sass.load_paths << File.expand_path(File.join(path, "..", 'vendor', 'assets', 'stylesheets', 'sencha-touch', 'default'))
   end
 end
 
