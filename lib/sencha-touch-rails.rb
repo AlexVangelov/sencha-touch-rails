30137271ef7a1f36f2543ac744c9c0526fe5b5cb
Eric Betts <bettse@gmail.com>
2013-06-23 14:47:04 -0700

    Upgrade to Sencha Touch 2.2
    I have no idea what I'm doing

--- lib/sencha-touch-rails.rb
+++ lib/sencha-touch-rails.rb
@@ -1,9 +1,11 @@
 require 'sencha-touch/rails'
 require 'theme_images'
+require 'inline_font'
 
 module SenchaTouch
   def self.load!
     path = File.expand_path File.dirname(__FILE__)
+    Sass.load_paths << File.expand_path(File.join(path, "..", 'vendor', 'assets', 'fonts'))
     Sass.load_paths << File.expand_path(File.join(path, "..", 'vendor', 'assets', 'stylesheets'))
     Sass.load_paths << File.expand_path(File.join(path, "..", 'vendor', 'assets', 'stylesheets', 'sencha-touch', 'default'))
   end
--- lib/sencha-touch/rails/version.rb
+++ lib/sencha-touch/rails/version.rb
@@ -1,6 +1,6 @@
 module SenchaTouch
   module Rails
-    VERSION = "1.1.3"
-    SENCHA_TOUCH_VERSION = "2.1.1"
+    VERSION = "1.1.4"
+    SENCHA_TOUCH_VERSION = "2.2.1"
   end
 end
